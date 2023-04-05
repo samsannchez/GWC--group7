33bb20344bead2d68c2f419f6370e5a45662be4c
sopgeo <sophiageorgieva@knights.ucf.edu>
2023-04-05 16:32:04 -0400

    added beginning of scene 5

--- adventuregame.py
+++ adventuregame.py
@@ -11,6 +11,7 @@ def typing(sentence):
         sys.stdout.flush()
         sleep(0.05)
         if(x == "\n"):
+            # press any key to continue?
             wait = input()
     
 
@@ -23,6 +24,12 @@ def dashboard_intro(location):
     #If using Windows os.system('cls')
     os.system('clear') 
     print("Location: ", location)
+
+# for intro scene: have user touch the amulet and have it glow so they know they have 
+# become the chosen one
+
+# maybe include a command sheet that the player has access to at all times
+# that includes the keys they need to press to navigate through the game
 
 location = "Beach" 
 dashboard_intro(location)
@@ -43,8 +50,62 @@ typing("Chief Koa: Thank heavens.\nOur village is facing its greatest crisis. \n
 typing("You are the only one who can save our village.\nYou have to build a boat to get to the Island.\nYou will need these to collect *insert#of logs* log, *#of vines* vines,fabric to build a boat.\n")
 typing("Take this backpack on your quest to store the items you collect along the way.")
 
+take = ["x"]
+print("Type 'x' to take the backpack.")
+
+
 #Finish conversation with the chief and start the quest
 
+
 #Do trades with characters in the forest.
+# Scene 5: Player encounters a new character and trades wood for a rope
+# Player is in the forest collecting wood. All of a sudden he hears footsteps, turns around
+# and sees Captain Jack, a retired pirate who now runs a bar on the beach. He has an eyepatch
+# and missing leg. You ask him if he has any rope. 
+
+def forest():
+    if userInput == "":
+        meetCaptainJack()
+
+# def trade():
+
+
+def talkToJack():
+    options = ["offer a trade", "walk away"]
+    typing("Hey there! I'm Captain Jack. I was a pirate until my ship got attacked. My good 'ol leg also went down with the ship.")
+    typing("\nNow I run a bar West of the Village. Come 'n stop by anytime!")
+    userInput = "";
+    # while userInput not in options:
+    #     print("\nOptions: offer a trade/walk away")
+    #     userInput = input()
+
+        # if userInput == "offer a trade"
+        #     trade()
+
+        # if userInput == "walk away"
+        #     forest()
+
+def meetCaptainJack():
+    location = "forest"
+    options = ["talk to jack", "ignore jack"]
+    typing("\nYou hear footsteps behind you.\n")
+    typing("You turn around and see Captain Jack, a retired pirate who now runs a bar on the beach.\n")
+    typing("He has an eyepatch and a missing leg. ")
+    typing("\nWhat would you like to do?")
+    userInput = ""
+    while userInput not in options:
+        print("\nOptions: talk to jack/ignore jack")
+        userInput = input()
+        if userInput == "talk to jack":
+            talkToJack()
+
+        if userInput == "ignore jack":
+            ignoreJack()
+
+meetCaptainJack()
+
+# Scene 6: Player encounters bad guys (threats) and fights them for fabric.
+
+
 
 
