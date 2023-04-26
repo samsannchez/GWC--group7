8c40f64a8aad5d572c91e98dab83cc4800583c84
Glory-SM <121260408+Glory-SM@users.noreply.github.com>
2023-04-26 16:38:04 -0400

    Add files via upload

---
+++ Scene_Island.py
@@ -0,0 +1,10 @@
+#Scene Island
+#Meet character Mana who explains the rest of the story
+#She is the care taker the person/island who lost the heart
+def Mana():
+    typing("Thank you for returning the heart\n")
+    typing("The amulet was the heart of the ....\n.Many years have passed since the heart was stolen\n")
+    typing("The heart began to lose its life force as it had spend far too long away from its body.\n")
+    typing("It began to consume the the villages life force to survive\n")
+    typing("Finally the heart as been returned to its orignal owner. The ... is at peace!\n")
+    typing("Enter ... to continue into the ...")
