4515f347af7dd3aa2a0b81808b69033025f790d6
sopgeo <sophiageorgieva@knights.ucf.edu>
2023-04-26 17:08:07 -0400

    removed statements for testing

--- inventory.py
+++ inventory.py
@@ -18,19 +18,11 @@ def remove_item(type, amount):
 	new_amount=mybackpack[type]-amount
 	mybackpack.update({type: new_amount})
 
-add_item("fruit", 100)
-print(mybackpack)
-remove_item("fruit", 50)
-print(mybackpack)
-
 #Remove all items from inventory 
 def remove_all():
 	mybackpack["wood"]=0
 	mybackpack["rope"]=0
 	mybackpack["fruit"]=0
-
-remove_all()
-print(mybackpack)
 
 #Display inventory
 def display_inventory():
