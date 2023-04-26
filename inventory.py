dde037a460f4f1966bfe9aa675e2a2213f4faab1
samsannchez <69266761+samsannchez@users.noreply.github.com>
2023-04-26 15:47:08 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,1 +1,22 @@
 
+import os
+import sys
+
+#Declaring dictionary
+wood=0
+rope=0
+fruit=0
+#Add to inventory
+
+#Remove item from inventory
+
+#Remove all items from inventory 
+
+#Display inventory
+def display_inventory():
+	os.system('clear')
+	print("----Backpack----")
+	print("wood: "+str(wood)+"\n")
+	print("rope: "+str(rope)+"\n")
+	print("fruit: "+str(fruit)+"\n")
+	goback=input()
