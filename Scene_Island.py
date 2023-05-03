9c37897121a96f96dd1625ac28785af4c92e62d3
Glory-SM <121260408+Glory-SM@users.noreply.github.com>
2023-05-03 14:27:28 -0400

    Update Scene_Island.py

--- Scene_Island.py
+++ Scene_Island.py
@@ -3,8 +3,8 @@
 #She is the care taker the person/island who lost the heart
 def Mana():
     typing("Thank you for returning the heart\n")
-    typing("The amulet was the heart of the ....\n.Many years have passed since the heart was stolen\n")
+    typing("The amulet was the heart of the island\n.Many years have passed since the heart was stolen\n")
     typing("The heart began to lose its life force as it had spend far too long away from its body.\n")
     typing("It began to consume the the villages life force to survive\n")
-    typing("Finally the heart as been returned to its orignal owner. The ... is at peace!\n")
+    typing("Finally the heart as been returned to its orignal owner. The island is at peace!\n")
     typing("Enter ... to continue into the ...")
