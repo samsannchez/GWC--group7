86ee9d0a39038efd9a5e071848671abc59ed5cfb
samsannchez <69266761+samsannchez@users.noreply.github.com>
2023-07-08 14:30:32 -0400

    Update scenes.py

--- scenes.py
+++ scenes.py
@@ -3,8 +3,8 @@ from time import sleep
 import os
 import sys
 import inventory
-from functions import typing, current_location, animation
-from inventory import display_inventory, display_character_inventory, add_item, remove_item, remove_all, mybackpack
+from functions import typing, current_location, animation, clear_screen
+from inventory import display_inventory, display_character_inventory, add_item, remove_item, remove_all, mybackpack, notmybackpack
 
 name=""
 
@@ -19,21 +19,22 @@ name=""
 # maybe include a command sheet that the player has access to at all times
 # that includes the keys they need to press to navigate through the game
 def scene1():
+    clear_screen()
     location = "Coral Cove Beach" 
     current_location("x","","","", 1)
     typing("An amulet has been swept up on the beach by the village.\nYou bend down to pick it up.\n")
     typing("Tiki, the local surfer, glances over at your direction.\nHe eyes the object in your hand.\n")
     typing("Tiki: Oi Mate! I haven't seen you around here before, what's your name?\n")
     name = input("Type your name:")
-    os.system('clear') 
+    clear_screen()
     typing("Tiki: Welcome to Coral Cove ")
     typing(name)
     typing("!\n")
     typing("I saw you found something by the shore.\nLet me take a closer look...\nI can't be belive my eyes, this amulet has been lost for years.\nWe need to tell the Chief!\n")
-    os.system('clear') 
+    clear_screen()
     typing("Tiki takes you to the village...\n")
     animation()
-    os.system('clear') 
+    clear_screen()
     current_location("x","","","", 1)
     typing("Tiki: Chief Koa! ")
     typing(name)
@@ -43,6 +44,7 @@ def scene1():
     typing("You are the only one who can save our village.\nYou will have to venture into the dark forest.\n")
     typing("Take this backpack on your quest to store the items you collect along the way.\n")
     backpack=input("Type 'x' to take the backpack.")
+    clear_screen()
      
 #Scene 3 :Player meets a new character that will lead him into the forest
 # The ranger informs you about the forest
@@ -54,25 +56,26 @@ def scene1():
 ######################################
 
 def Ranger():
-    typing("I'm the forest ranger, my family has looked after the forest for many generation.\n")
+    clear_screen()
+    typing("Walking to the dark forest...\n")
+    animation()
+    clear_screen()
+    current_location(" ","x","","", 2)
+    typing("I'm the forest ranger, my family has looked after the forest for many generations.\n")
     typing("There are still many unknown mysteries of the forest yet to be discovered.\n")
-    typing("For your quest you will need the strongest wood to cross the sea.\n")
+    typing("For your quest you will need the strongest wood.\n")
     typing("Beware the strongest wood lies deep within the forest, where unknown creatures lurk.\n")
     typing("Many strong people have ventured into the deep forest and have not returned.\n")
-    typing("The Chief said only you can save our our village. \nI wish you a safe journey ")
+    typing("I wish you a safe journey ")
     typing(name)
     typing("!\n")
-
-    userinput=typing("Enter 2 to continue into the forest")
-    if (userinput == "2"):
-        typing("Walking to the dark forest...\n")
-        animation()
-        current_location("","x","","",2)
+    clear_screen()
 
 # def ignoreJack():
 #     print("User ignored Jack")
 
 def talkToJack():
+    clear_screen()
     options = ["y", "n"]
     # typing("Hey there! I'm Captain Jack. I was a pirate until my ship got attacked. My good 'ol leg also went down with the ship.")
     # typing("\nNow I run a bar West of the Village.")
@@ -86,9 +89,10 @@ def talkToJack():
 
         if userInput == "n":
             display_inventory()
+    clear_screen()
 
 def trade(character):
-    os.system('clear')
+    clear_screen()
     display_character_inventory(character)
     print("\n")
     display_inventory()
@@ -121,7 +125,7 @@ def trade(character):
     #         add_item(userInput, amount)
     #     print("That's not a valid item.")
 
-    os.system('clear')
+    clear_screen()
     display_character_inventory(character)
     print("\n")
     display_inventory()
@@ -130,11 +134,12 @@ def trade(character):
 
 
 def meetCaptainJack():
+    clear_screen()
     location = "forest"
     options = ["t", "i"]
-    # typing("You hear footsteps behind you.\n")
-    # typing("You turn around and see Captain Jack, a retired pirate who now runs a bar on the beach.\n")
-    # typing("He has an eyepatch and a missing leg. ")
+    typing("You hear footsteps behind you.\n")
+    typing("You turn around and see Captain Jack, a retired pirate who now runs a bar on the forest.\n")
+    typing("He has an eyepatch and a missing leg.\n")
     typing("What would you like to do?")
     userInput = ""
     while userInput not in options:
@@ -152,6 +157,7 @@ def meetCaptainJack():
 
 
 def Mana():
+    clear_screen()
     typing("Thank you for returning the heart\n")
     typing("The amulet was the heart of the island\n.Many years have passed since the heart was stolen\n")
     typing("The heart began to lose its life force as it had spend far too long away from its body.\n")
